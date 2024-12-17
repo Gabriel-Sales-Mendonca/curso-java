16ad78beb344e3803df4732e1db5e5acbc2fac14
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-12-16 21:40:48 -0300

    Realizei a conexão com o banco de dados

--- project_19/src/Main.java
+++ project_19/src/Main.java
@@ -1,5 +1,10 @@
+import db.DB;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
     }
 }

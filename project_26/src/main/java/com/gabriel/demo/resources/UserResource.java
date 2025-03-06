49b400a44dbdeee2c02f4bf38cf0bf844e75a559
Gabriel Sales <gabrielsales07112018@gmail.com>
2025-03-05 22:44:51 -0300

    Criei a rota para criar um usuário

--- project_26/src/main/java/com/gabriel/demo/resources/UserResource.java
+++ project_26/src/main/java/com/gabriel/demo/resources/UserResource.java
@@ -4,11 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.gabriel.demo.entities.User;
 import com.gabriel.demo.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +27,12 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User response = service.findById(id);
         return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }

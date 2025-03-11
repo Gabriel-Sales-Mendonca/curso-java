2afcfb6c8ca7cea89d45f8c3e6e60f8b8517bc27
Gabriel Sales <gabrielsales07112018@gmail.com>
2025-03-11 08:03:56 -0300

    Criei a exceção personalizada para a rota delete

--- project_26/src/main/java/com/gabriel/demo/services/UserService.java
+++ project_26/src/main/java/com/gabriel/demo/services/UserService.java
@@ -1,9 +1,12 @@
 package com.gabriel.demo.services;
 
+import com.gabriel.demo.services.exceptions.DataBaseException;
 import com.gabriel.demo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.gabriel.demo.entities.User;
 import com.gabriel.demo.repositories.UserRepository;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,15 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DataBaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
